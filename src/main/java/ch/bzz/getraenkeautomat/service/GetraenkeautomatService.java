98badd75d4ae6e36bfc5760d3fd2d921a547e16e
Mirco Di Natale <dinatalem@bzz.ch>
2022-05-23 21:00:21 +0200

    23.05.2022 / 20:15

--- src/main/java/ch/bzz/getraenkeautomat/service/GetraenkeautomatService.java
+++ src/main/java/ch/bzz/getraenkeautomat/service/GetraenkeautomatService.java
@@ -1,4 +1,27 @@
 package ch.bzz.getraenkeautomat.service;
 
+import ch.bzz.getraenkeautomat.data.DataHandler;
+import ch.bzz.getraenkeautomat.model.Getraenkeautomat;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.awt.*;
+import java.util.List;
+
+@Path("getraenkeautomat")
 public class GetraenkeautomatService {
+    @Path("list")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listGetraenkeautomat() {
+        List<Getraenkeautomat> getraenkeautomatList = DataHandler.getInstance().readAllGetraenkeautomaten();
+        Response response = Response
+                .status(200)
+                .entity(getraenkeautomatList)
+                .build();
+        return response;
+    }
 }
--- src/main/java/ch/bzz/getraenkeautomat/service/MarkeService.java
+++ src/main/java/ch/bzz/getraenkeautomat/service/MarkeService.java
@@ -1,4 +1,27 @@
 package ch.bzz.getraenkeautomat.service;
 
+import ch.bzz.getraenkeautomat.data.DataHandler;
+import ch.bzz.getraenkeautomat.model.Marke;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.awt.*;
+import java.util.List;
+
+@Path("marke")
 public class MarkeService {
+    @Path("list")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listMarke() {
+        List<Marke> markeList = DataHandler.getInstance().readAllMarken();
+        Response response = Response
+                .status(200)
+                .entity(markeList)
+                .build();
+        return response;
+    }
 }
